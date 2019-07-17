f25c6688064778b5b11f0edab492dd890d1eb835
sandisomadayi <sandisomadayi239@gmail.com>
2019-07-17 11:30:04 +0200

    added gitIgnore file

--- test/soccer/java/soccer/PlayerTest.java
+++ test/soccer/java/soccer/PlayerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlayerTest {
+
     @Test
     public void shouldGetPlayerName() {
         Player player1 = new Player("sandiso", "madayi", 96);
