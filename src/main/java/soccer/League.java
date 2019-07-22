8a6819321c4ac372e4451ebbd898e5f9cfbde0c8
Sandiso Madayi <sandisomadayi239@gmail.com>
2019-07-22 07:38:55 +0200

    added teams, players and began a match

--- src/main/java/soccer/League.java
+++ src/main/java/soccer/League.java
@@ -3,9 +3,12 @@ package soccer;
 public class League {
     public static void main(String[] args) {
         //TODO
-        Player player1 = new Player("sandiso", "madayi", 96);
-        Player player2 = new Player("mad", "man", 76);
-        Player player3 = new Player("min", "max", 10);
+        Player player1 = new Player("Sandiso", "Madayi", 96);
+        Player player2 = new Player("Mad", "Man", 76);
+        Player player3 = new Player("Min", "Max", 10);
+
+//        player1.firstName = "sandiso";
+//        player2.firstName = "sandman";
 
         Player[] thePlayers = {player1, player2, player3};
 
@@ -13,22 +16,30 @@ public class League {
         Team team2 = new Team();
         Team team3 = new Team();
 
-        team1.teamName = "chiefs";
-        team2.teamName = "pirates";
-        team3.teamName = "sundowns";
+        team1.teamName = "Chiefs";
+        team2.teamName = "Pirates";
+        team3.teamName = "Sundowns";
 
         team1.teamPlayers = thePlayers;
+        team2.teamPlayers = new  Player[3];
+
+        team2.teamPlayers[0] = new Player("Robert", "Service", 15);
+        team2.teamPlayers[1] = new Player("Robbie", "Burns", 14);
+        team2.teamPlayers[2] = new Player("Rafael", "Sabatini", 11);
 
         Team[] teams = {team1, team2, team3};
 
-        for (Player player : team1.teamPlayers) {
-            System.out.println(player.firstName);
-        }
+        Game currentGame = new Game();
+        currentGame.homeTeam = team1;
+        currentGame.awayTeam = team2;
 
-        System.out.println("--------------------------------------------------");
+        Goal firstGoal = new Goal();
 
-        for (Team team: teams) {
-            System.out.println(team.teamName);
-        }
+        firstGoal.thePlayer = currentGame.homeTeam.teamPlayers[2];
+        firstGoal.theTeam = currentGame.homeTeam;
+        firstGoal.theTime = 55;
+
+        Goal[] theGoals = {firstGoal};
+        currentGame.goals = theGoals;
     }
 }
